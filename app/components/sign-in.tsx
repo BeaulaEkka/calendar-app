a94b3d77de4f59b6aba824b09b27c5c3d4d504f5
Your Name <ekka.beaula@gmail.com>
2024-10-02 10:41:42 +0200

    github google auth setup

---
+++ app/components/sign-in.tsx
@@ -0,0 +1,19 @@
+
+import { signIn } from "@/auth"
+ 
+export default function SignIn() {
+  return (
+    <form
+      action={async () => {
+        "use server"
+        await signIn("github")
+         await signIn("google")
+      }}
+
+      
+    >
+      <button type="submit">Signin with GitHub</button>
+       <button type="submit">Signin with Google</button>
+    </form>
+  )
+} 
