cf65679a6e9f665476dc06273507c6a5bbc7a9db
Your Name <ekka.beaula@gmail.com>
2024-10-03 23:55:28 +0200

    dashboard links test

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -5,5 +5,5 @@ export default async function Dashboard() {
   const session = await requireUser();
   console.log("session", session);
 
-  return <div>this is dashboard page</div>;
+  return <div>this is dashboard page text</div>;
 }
