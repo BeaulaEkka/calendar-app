f3148c76fd0e45139d278cf8e2d808672165a0a5
Your Name <ekka.beaula@gmail.com>
2024-10-04 22:55:22 +0200

    usePath setup

--- app/components/DashboardLinks.tsx
+++ app/components/DashboardLinks.tsx
@@ -1,5 +1,8 @@
+"use client";
+import { cn } from "@/lib/utils";
 import { HomeIcon, LucideProps, Settings, User2 } from "lucide-react";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 
 interface iAppProps {
@@ -33,14 +36,24 @@ export const dashboardLinks: iAppProps[] = [
 ];
 
 export default function DashboardLinks() {
+  const pathname = usePathname();
   return (
-    <>
+    <div className="px-4 py-2">
       {dashboardLinks.map((link) => (
-        <Link key={link.id} href={link.href}>
+        <Link
+          key={link.id}
+          href={link.href}
+          className={cn(
+            "flex gap-2 items-center py-3 hover:bg-gray-200 p-2 rounded-sm",
+            pathname === link.href
+              ? "text-primary bg-primary-500"
+              : "text-muted-foreground"
+          )}
+        >
+          <link.icon className="size-4 text-orange-500 font-extrabold" />
           {link.name}
-          <link.icon className="size-4" />
         </Link>
       ))}
-    </>
+    </div>
   );
 }
